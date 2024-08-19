b379e607ba1e763565932d5d2a97e4d3c3d98ff0
Simon Pawlowski <simon.pawlowski@aytee.de>
2024-08-19 12:17:00 +0200

    Unique Identifier Property for Abstract Scene Class
    OBS does not use Id Property, Streamlabs does.
    The correct identifier is now handled by each individual class.

--- AyteeDE.StreamAdapter/Entities/External/OBSWebsocket5/OBSStudioWebsocket5Scene.cs
+++ AyteeDE.StreamAdapter/Entities/External/OBSWebsocket5/OBSStudioWebsocket5Scene.cs
@@ -4,14 +4,13 @@ namespace AyteeDE.StreamAdapter.Entities.External;
 
 public class OBSStudioWebsocket5Scene : Scene, IEquatable<OBSStudioWebsocket5Scene>
 {
-    public string ID { get; init; }
     public override string Name { get ; set; }
-
+    public override string UniqueIdentifier => Name;
     public bool Equals(OBSStudioWebsocket5Scene? other)
     {
         if(other != null)
         {
-            return this.ID == other.ID;
+            return this.Name == other.Name;
         }
 
         return false;
--- AyteeDE.StreamAdapter/Entities/StreamAdapter/Scene.cs
+++ AyteeDE.StreamAdapter/Entities/StreamAdapter/Scene.cs
@@ -3,6 +3,7 @@
 public abstract class Scene
 {
     public abstract string Name { get; set; }
+    public abstract string UniqueIdentifier { get; }
     public override string ToString()
     {
         return Name;
