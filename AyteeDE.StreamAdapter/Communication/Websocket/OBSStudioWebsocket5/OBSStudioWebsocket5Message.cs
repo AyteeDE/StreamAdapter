81823b5a84306af92f4953da5740825eb328c0f8
Simon Pawlowski <simon.pawlowski@aytee.de>
2024-08-19 14:39:27 +0200

    Create Message Class for OBSWebsocket 5

---
+++ AyteeDE.StreamAdapter/Communication/Websocket/OBSStudioWebsocket5/OBSStudioWebsocket5Message.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AyteeDE.StreamAdapter;
+
+public class OBSStudioWebsocket5Message
+{
+    public int Op { get; set; }
+    public OBSStudioWebsocket5MessageData D { get; set; }
+}
+public class OBSStudioWebsocket5MessageData
+{
+    public string ObsWebsocketVersion { get; set; }
+    public int? RpcVersion { get; set; }
+    public int? NegotiatedRpcVersion { get; set; }
+    public object Authentication { get; set; }
+    [JsonIgnore]
+    public string AuthenticationString
+    {
+        get
+        {
+            return Authentication.ToString();
+        }
+    }
+    [JsonIgnore]
+    public OBSStudioWebsocket5MessageAuthenticationData AuthenticationData
+    {
+        get
+        {
+            return JsonSerializer.Deserialize<OBSStudioWebsocket5MessageAuthenticationData>(AuthenticationString);
+        }
+    }
+    public int? EventSubscriptions { get; set; }
+    public int? EventIntent { get; set; }
+    public string EventType { get; set; }
+    public OBSStudioWebsocket5MessageEventData EventData { get; set; }
+    public string RequestType { get; set; }
+    public string RequestId { get; set; }
+    public OBSStudioWebsocket5MessageRequestData RequestData { get; set; }
+    public OBSStudioWebsocket5MessageRequestStatus RequestStatus { get; set; }
+    public OBSStudioWebsocket5MessageResponseData ResponseData { get; set; }
+}
+public class OBSStudioWebsocket5MessageAuthenticationData
+{
+    public string Challenge {get;set;}
+    public string Salt {get;set;}
+}
+public class OBSStudioWebsocket5MessageEventData
+{
+    public string SceneName {get;set;}
+}
+public class OBSStudioWebsocket5MessageRequestData
+{
+    public string SceneName {get;set;}
+}
+public class OBSStudioWebsocket5MessageRequestStatus
+{
+    public int? Code {get;set;}
+    public bool? Result {get;set;}
+}
+public class OBSStudioWebsocket5MessageResponseData
+{
+    public string CurrentProgramSceneName {get;set;}
+    public string CurrentPreviewSceneName {get;set;}
+    public List<OBSStudioWebsocket5MessageScene> Scenes {get;set;}
+}
+public class OBSStudioWebsocket5MessageScene
+{
+    public int? SceneIndex {get;set;}
+    public string SceneName {get;set;}
+}
