f5ab8965bf15e23274f4140a89854164b445bbf5
Simon Pawlowski <simon.pawlowski@aytee.de>
2024-08-19 14:37:58 +0200

    Disable async warning

--- AyteeDE.StreamAdapter/Communication/Websocket/WebsocketConnection.cs
+++ AyteeDE.StreamAdapter/Communication/Websocket/WebsocketConnection.cs
@@ -30,7 +30,9 @@ public class WebsocketConnection
             }
         }
 
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         ListenAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         return IsConnected;
     }
     public async Task<bool> DisconnectAsync()
