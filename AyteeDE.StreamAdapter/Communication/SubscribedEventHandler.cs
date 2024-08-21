59b1114d3b0014dadd6fa9d387368a2018ee7280
Simon Pawlowski <simon.pawlowski@aytee.de>
2024-08-21 11:26:50 +0200

    Create SubscribedEventHandler.cs
    Static function to check if the handler is subscribed before invoking

---
+++ AyteeDE.StreamAdapter/Communication/SubscribedEventHandler.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AyteeDE.StreamAdapter.Communication;
+
+public static class SubscribedEventHandler
+{
+    public static void InvokeSubscribedEvent<T>(EventHandler<T> eventHandler, object sender, T args)
+    {
+        if(eventHandler != null)
+        {
+            eventHandler.Invoke(sender, args);
+        }
+    }
+}
